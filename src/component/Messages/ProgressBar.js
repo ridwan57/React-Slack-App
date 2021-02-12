4539258b745a4c5d4c55f552e3a344075b02ef5e
ridwan57 <ridwananik57@gmail.com>
2021-02-13 05:45:32 +0600

    uploading image

---
+++ src/component/Messages/ProgressBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Progress } from 'semantic-ui-react'
+
+const ProgressBar = ({ uploadState, percentUploaded }) => (
+    uploadState && (
+        <Progress
+            className='progress__bar'
+            percent={percentUploaded}
+            indicating
+            progress
+            size='medium'
+            inverted
+        />
+    )
+)
+export default ProgressBar
