21a74df3dc9c3ca8fea5cca526ce1063ab0c227a
Ridwan <ridwananik57@gmail.com>
2021-05-09 05:16:14 +0600

    App functional refactored

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import "./App.css";
 import ColorPanel from "./ColorPanel/ColorPanel";
@@ -6,48 +6,64 @@ import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 import SidePanel from "./SidePanel/SidePanel";
 
-const App = ({ primaryColor, secondaryColor, currentUser, currentChannel, isPrivateChannel, userPosts }) => (
-  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
-    <ColorPanel
-      key={currentUser && currentUser.name}
-      currentUser={currentUser}
-    // primaryColor={primaryColor}
-    // secondaryColor={secondaryColor}
-    />
-    <SidePanel
-      key={currentUser && currentUser.uid}
-      currentUser={currentUser}
-      primaryColor={primaryColor} />
+const App = () => {
+  const { currentUser } = useSelector((state) => ({ ...state.user }));
 
+  const {
+    currentChannel,
+    isPrivateChannel,
+    userPosts,
+  } = useSelector((state) => ({ ...state.channel }));
 
-    <Grid.Column style={{ marginLeft: 320 }} >
-      <Messages
-        key={currentChannel && currentChannel.id}
-        currentChannel={currentChannel}
+  const { primaryColor, secondaryColor } = useSelector((state) => ({
+    ...state.colors,
+  }));
+
+  return (
+    <Grid
+      columns="equal"
+      className="app"
+      style={{ background: secondaryColor }}
+    >
+      <ColorPanel
+        key={currentUser && currentUser.name}
         currentUser={currentUser}
-        isPrivateChannel={isPrivateChannel}
+        // primaryColor={primaryColor}
+        // secondaryColor={secondaryColor}
       />
-    </Grid.Column>
-
-    <Grid.Column width="4" >
-      <MetaPanel
-        key={currentChannel && currentChannel.name}
-        isPrivateChannel={isPrivateChannel}
-        currentChannel={currentChannel}
-        userPosts={userPosts}
+      <SidePanel
+        key={currentUser && currentUser.uid}
+        currentUser={currentUser}
+        primaryColor={primaryColor}
       />
-    </Grid.Column>
 
-  </Grid>
-)
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
-  currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel,
-  userPosts: state.channel.userPosts,
-  primaryColor: state.colors.primaryColor,
-  secondaryColor: state.colors.secondaryColor,
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages
+          key={currentChannel && currentChannel.id}
+          currentChannel={currentChannel}
+          currentUser={currentUser}
+          isPrivateChannel={isPrivateChannel}
+        />
+      </Grid.Column>
 
-})
+      <Grid.Column width="4">
+        <MetaPanel
+          key={currentChannel && currentChannel.name}
+          isPrivateChannel={isPrivateChannel}
+          currentChannel={currentChannel}
+          userPosts={userPosts}
+        />
+      </Grid.Column>
+    </Grid>
+  );
+};
+// const mapStateToProps = (state) => ({
+//   currentUser: state.user.currentUser,
+//   currentChannel: state.channel.currentChannel,
+//   isPrivateChannel: state.channel.isPrivateChannel,
+//   userPosts: state.channel.userPosts,
+//   primaryColor: state.colors.primaryColor,
+//   secondaryColor: state.colors.secondaryColor,
+// });
 
-export default connect(mapStateToProps)(App);
+export default App;
